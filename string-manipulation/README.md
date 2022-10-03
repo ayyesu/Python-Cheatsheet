60411bb72574576e46649f88345051948cd6c54e
Daniel Kissiedu <iamdanielkissiedu@gmail.com>
2022-10-03 21:24:05 +0000

    Working with strings

---
+++ string-manipulation/README.md
@@ -0,0 +1,3 @@
+<h1>string Manipulation</h1>
+<hr>
+This repository contains various ways to create string outputs using the print function.
---
+++ string-manipulation/string.py
@@ -0,0 +1,7 @@
+#Outputs Hello world on different lines
+print("Hello world\nHello world")
+
+#string Concactination
+#Combining two or more strings 
+print("Welcome" + " to my repository")
+print("Happy" + " " + "Learing")
