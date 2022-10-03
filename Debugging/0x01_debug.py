a56e429628a5fbc5bddf3289ced76a5863db2c8f
Daniel Kissiedu <iamdanielkissiedu@gmail.com>
2022-10-03 21:25:13 +0000

    Getting rid of bugs

---
+++ Debugging/0x01_debug.py
@@ -0,0 +1,11 @@
+#print("Day1- Python print function..)
+#prit("The function declares like this:")
+#   print("print('whatever you want to print')")
+
+#We'll be fixing the code above:
+
+print("Day1- Python print function..")
+print("The function declares like this:")
+print("print('whatever you want to print')")
+
+#Yay the code has been fixed!!
---
+++ Debugging/README.md
@@ -0,0 +1,3 @@
+<h1>Debugging</h1>
+<hr>
+In this repository we'll learn how to <b>fix errors(debug)</b> our code.
